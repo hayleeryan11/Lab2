51d0371dd8fbdb3f02594656c46070ade80f58e4
jmatthewfrazier <jmf87@uw.edu>
2018-04-06 20:02:57 -0700

    added a spooky button

---
+++ SpookyBois/app/src/main/java/edu/tacoma/uw/css/haylee11/spookybois/DisplayMattsActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.haylee11.spookybois;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMattsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_matts);
+    }
+}
