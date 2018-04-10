032b3dddf195be437365c6dede6bbe96eaa20d34
unknown <kai.n.stansfield@gmail.com>
2018-04-10 12:31:42 -0700

    commitment

--- SpookyBois/app/src/main/java/edu/tacoma/uw/css/haylee11/spookybois/MainActivity.java
+++ SpookyBois/app/src/main/java/edu/tacoma/uw/css/haylee11/spookybois/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.tacoma.uw.css.haylee11.spookybois;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void kaiMethod(View view) {
+        Intent intent = new Intent(this, imageActivity.class);
+        startActivity(intent);
     }
 }
---
+++ SpookyBois/app/src/main/java/edu/tacoma/uw/css/haylee11/spookybois/imageActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.haylee11.spookybois;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class imageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
