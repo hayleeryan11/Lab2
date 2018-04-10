55d304323929b893e901a596e18398e44687c76a
unknown <kai.n.stansfield@gmail.com>
2018-04-10 13:50:32 -0700

    New personal portion, hopefully works

---
+++ SpookyBois/app/src/main/java/edu/tacoma/uw/css/haylee11/sppokybois/ImageActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.css.haylee11.sppokybois;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
